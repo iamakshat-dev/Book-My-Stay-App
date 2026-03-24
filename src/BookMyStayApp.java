a6c91156b650cb331bf8306ac500e6bb8b865e28
iamakshat-dev <iamakshat2304@gmail.com>
2026-03-24 14:33:33 +0530

    UC2:Basic Room Types & Static Availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,18 +1,93 @@
-
+// Main Class
 public class BookMyStayApp {
-
 
     public static void main(String[] args) {
 
-        // Application name and version
-        String appName = "Hotel Booking Management System";
+        // Create room objects (Polymorphism)
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
 
+        // Static availability
+        int singleAvailability = 5;
+        int doubleAvailability = 3;
+        int suiteAvailability = 2;
 
-        // Display welcome message
-        System.out.println("Welcome to the " + appName);
+        System.out.println("Hotel Room Initialization\n");
 
+        // Display details
+        single.displayRoomDetails();
+        System.out.println("Available: " + singleAvailability);
+        System.out.println();
 
-        // Confirmation message
-        System.out.println("System initialized successfully.");
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available: " + doubleAvailability);
+        System.out.println();
+
+        suite.displayRoomDetails();
+        System.out.println("Available: " + suiteAvailability);
+    }
+}
+
+// Abstract Class
+abstract class Room {
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
+
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public abstract void displayRoomDetails();
+}
+
+// Single Room Class
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super(1, 250, 1500.0);
+    }
+
+    @Override
+    public void displayRoomDetails() {
+        System.out.println("Single Room:");
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sqft");
+        System.out.println("Price per night: " + pricePerNight);
+    }
+}
+
+// Double Room Class
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super(2, 400, 2500.0);
+    }
+
+    @Override
+    public void displayRoomDetails() {
+        System.out.println("Double Room:");
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sqft");
+        System.out.println("Price per night: " + pricePerNight);
+    }
+}
+
+// Suite Room Class
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super(3, 750, 5000.0);
+    }
+
+    @Override
+    public void displayRoomDetails() {
+        System.out.println("Suite Room:");
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sqft");
+        System.out.println("Price per night: " + pricePerNight);
     }
 }
